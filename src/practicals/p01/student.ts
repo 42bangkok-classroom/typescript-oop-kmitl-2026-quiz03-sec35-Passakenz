0b3aec823fba9cc0299f27123fc8106d0858d2d0
Passakenz <ken.ods.555@gmail.com>
2026-02-16 10:11:13 +0700

    check2

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,26 +1,11 @@
-class student {
-    firstname: string
-    lastname:string
+export class Student {
+    public firstname: string
+    public lastname:string
 
-    constructor(firstname: string, lastname: string){
+    constructor(firstname: string = "", lastname: string = ""){
         this.firstname = firstname
         this.lastname = lastname
     }
-    printname(){
-        console.log(this.firstname + ' ' + this.lastname)
-    }
-}
-
-
-export class Student {
-    firstname: string = ''
-    lastname: string = ''
-
-    constructor(firstname?: string, lastname?: string){
-        if (firstname) this.firstname = firstname
-        if (lastname) this.lastname = lastname
-    }
-
     printName(){
         console.log(this.firstname + ' ' + this.lastname)
     }
