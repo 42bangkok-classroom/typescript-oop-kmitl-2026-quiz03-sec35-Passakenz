8084a43218e7cec8a7ce9cdfd93d4beb8e6ea377
Passakenz <ken.ods.555@gmail.com>
2026-02-16 09:55:56 +0700

    check1

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,27 @@
-export class Student {}
+class student {
+    firstname: string
+    lastname:string
+
+    constructor(firstname: string, lastname: string){
+        this.firstname = firstname
+        this.lastname = lastname
+    }
+    printname(){
+        console.log(this.firstname + ' ' + this.lastname)
+    }
+}
+
+
+export class Student {
+    firstname: string = ''
+    lastname: string = ''
+
+    constructor(firstname?: string, lastname?: string){
+        if (firstname) this.firstname = firstname
+        if (lastname) this.lastname = lastname
+    }
+
+    printName(){
+        console.log(this.firstname + ' ' + this.lastname)
+    }
+}
